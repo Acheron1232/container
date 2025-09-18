1a251d9097b0b631f30f37b3006b337780e96f2e
MykytaDanech <nikita.danech@gmail.com>
2025-09-18 17:26:16 +0300

    controller annotation swapped on RestController annotation

--- kitchen-server/src/main/java/com/mykyda/kitchenserver/http/controller/PizzaOrderController.java
+++ kitchen-server/src/main/java/com/mykyda/kitchenserver/http/controller/PizzaOrderController.java
@@ -3,15 +3,15 @@ package com.mykyda.kitchenserver.http.controller;
 import com.mykyda.kitchenserver.service.KitchenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/kitchen")
 public class PizzaOrderController {
@@ -19,7 +19,7 @@ public class PizzaOrderController {
     private final KitchenService kitchenService;
 
     @PatchMapping("/change-ready-quantity")
-    public ResponseEntity<Object> changeReadyQuantity(@RequestBody Map<UUID, Integer> pizzaOrdersReady) {
+    public ResponseEntity<String> changeReadyQuantity(@RequestBody Map<UUID, Integer> pizzaOrdersReady) {
         kitchenService.setReadyQuantities(pizzaOrdersReady);
         return ResponseEntity.noContent().build();
     }
